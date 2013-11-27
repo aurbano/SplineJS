a38eac9706da77d03a224bd23a5534bd3c0e7c46
aurbano <alejandro@urbanoalvarez.es>
2013-11-27 15:02:50 +0100

    Improved library code and demo

---
+++ src/splinejs.js
@@ -0,0 +1,188 @@
+/*!
+ * SplineJS JavaScript Library v0.0.1
+ * https://github.com/aurbano/SplineJS
+ *
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ */
+(function() {
+	// "use strict";
+	
+		// Reference to global object
+	var root = this,
+		// Previous value of SplineJS just in case
+		previousSpline = root.Spline,
+		// Create local references to array methods we'll want to use later.
+		array = [],
+		push = array.push,
+		slice = array.slice,
+		splice = array.splice,
+		// Top-level namespace. It will get exported
+		Spline;
+		
+	if(typeof exports !== 'undefined'){
+		Spline = exports;
+	}else{
+		Spline = root.Spline = {};
+	}
+	
+	// Current version
+	Spline.VERSION = '0.0.1';
+	
+	// noConflict mode
+	Spline.noConflict = function(){
+		root.Spline = previousSpline;
+		return this;
+	};
+	
+	/**
+	 * Generates a row vector y of n points linearly spaced between and including a and b. For n = 1, linspace returns b.
+	 * @param {int} Initial point
+	 * @param {int} Final point
+	 * @param {int} Number of points to return
+	 * @return {Array} Row vector of n points between a and b (included) 
+	 */
+	Spline.linspace = function(a,b,n){
+		if(typeof(n)=='undefined') n = 100;
+		if(n==1) return b;
+		var ret = [],
+			inc = (b-a)/(n-1);
+	    for (var i = a; i <= b; i+=inc) {
+	    	ret.push(i);
+	    }
+	    // Just in case the rounding was different
+	    ret[n-1] = b;
+	    return ret;
+	};
+	
+	/**
+	 * Create a new figure in the specified selector
+	 * @param {String} selector
+	 */
+	Spline.figure = function(selector, xValues){
+		var w = $(selector).width(),
+			h = $(selector).height(),
+			x = d3.scale.linear().domain([d3.min(xValues), d3.max(xValues)]).range([0, w]);
+			
+		console.log([d3.min(xValues), d3.max(xValues)]);
+		
+		var graph = d3.select(selector)
+			.append("svg:svg")
+				//.attr('preserveAspectRatio','xMinYMin')
+				.attr("viewBox", "0 0 " + (w+70) + " " + (h+30) )
+				//.attr("preserveAspectRatio", "xMidYMid")
+				.attr("preserveAspectRatio", "none")
+			.append("g")
+				.attr("transform", "translate(" + 50 + "," + 10 + ")");
+		
+		// Create x axis
+		var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+		// Add the x-axis.
+		graph.append("svg:g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + h + ")")
+		      .call(xAxis);
+				      
+		return {
+			name: name,
+			selector: selector,
+			w: w,
+			h: h,
+			g: graph
+		};
+	};
+	
+	/**
+	 * Wrapper for D3js line 
+	 */
+	Spline.line = d3.svg.line()
+		.x(function(d) { return d.x; })
+        .y(function(d) { return d.y; })
+        .interpolate("linear");
+	
+	/**
+	 * Plot data into a figure.
+	 * 
+	 * @param {Array} x values
+	 * @param {Array} y values
+	 * @param {Object} linespec: Line parameters (Color and thickness)
+	 */
+	Spline.plot = function(figure, xValues, yValues, style, color){
+		// y scale
+		var y = d3.scale.linear().domain([d3.min(yValues), d3.max(yValues)]).range([figure.h, 0]);
+		
+		var yAxis = d3.svg.axis().scale(y).ticks(6).orient("left");
+		// Add the y-axis to the left
+		figure.g.append("svg:g")
+		      .attr("class", "y axis")
+		      .attr("transform", "translate(-25,0)")
+		      .call(yAxis);
+		      
+		figure.g.append("svg:path").attr("d", Spline.line(Spline.formatData(xValues, yValues))).attr("stroke", color);
+	};
+	
+	/**
+	 * Convert data to the format expected by the plotters 
+	 */
+	Spline.formatData = function(x, y){
+		var ret = [];
+		for(var i=0; i<y.length; i++){
+			ret.push({x: x[i], y:y[i]});
+		}
+		return ret;
+	};
+	
+	/**
+	 * Gauss-Jordan matrix solving
+	 * @author Ivan Kuckir <http://blog.ivank.net> 
+	 */
+	Spline.gaussJ = {
+		zerosMat : function(r,c) {
+			var A = [];
+			for(var i=0; i<r; i++) {
+				A.push([]);
+				for(var j=0; j<c; j++)
+					A[i].push(0);
+			}
+			return A;
+		},
+		swapRows : function(m, k, l) {
+			var p = m[k];
+			m[k] = m[l];
+			m[l] = p;
+		},
+		_debug_printMat : function(A){
+			for(var i=0; i<A.length; i++)
+				console.log(A[i]);
+		},
+		solve : function(A, x){	// in Matrix, out solutions
+			var m = A.length;
+			for(var k=0; k<m; k++){	// column
+				// pivot for column
+				var i_max = 0; var vali = Number.NEGATIVE_INFINITY;
+				for(var i=k; i<m; i++) if(A[i][k]>vali) { i_max = i; vali = A[i][k];}
+				CSPL._gaussJ.swapRows(A, k, i_max);
+				
+				if(A[i_max][i] == 0) console.log("matrix is singular!");
+				
+				// for all rows below pivot
+				for(var i=k+1; i<m; i++){
+					for(var j=k+1; j<m+1; j++)
+						A[i][j] = A[i][j] - A[k][j] * (A[i][k] / A[k][k]);
+					A[i][k] = 0;
+				}
+			}
+			
+			for(var i=m-1; i>=0; i--){	// rows = columns
+				var v = A[i][m] / A[i][i];
+				x[i] = v;
+				for(var j=i-1; j>=0; j--){	// rows
+					A[j][m] -= A[j][i] * v;
+					A[j][i] = 0;
+				}
+			}
+		}
+	};
+	
+}).call(this);
